38da4f6b4873b5df52cafe7e96e45baabbe9bc67
fazeelcodes2012 <fazeelff96@gmail.com>
2026-08-05 00:16:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+/// Create an array ///
+let vegetables = ["Cucumber", "Tomato", "Carrot" , "Bengun" ,"potato"];
+console.log(vegetables);
+
+/// Update an array element///
+vegetables [1] = "onion";
+console.log(vegetables);
+
+/// Show specific value ///
+console.log(vegetables [0]);
+console.log(vegetables [2]);
